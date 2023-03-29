0e772d59fc66d83af240f6d86f8e90a4f74e1df8
permana017 <ahdipermana07@gmail.com>
2023-03-30 00:57:54 +0700

    feature: matrixs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes , Route, Navigate, redirect } from 'react-router-dom';
 import Detail from "./pages/Detail";
+import Matrix from "./pages/Matrix";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/detail" element={<Detail/>} />
+          <Route path="/matrix" element={<Matrix/>} />
       </Routes>
       </Router>
     );
